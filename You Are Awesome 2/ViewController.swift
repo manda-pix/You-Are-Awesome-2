1cd2457e58bdcb3c915825f9796489ef2a6c41c2
Amanda Guinyard <manda4066@gmail.com>
2021-06-08 13:53:22 -0400

    Section 2.8 If statements

--- You Are Awesome 2/ViewController.swift
+++ You Are Awesome 2/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageBox: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        
+        let awesomeMessage = "You are Awesome"
+        let greatMessage = "You are Great"
+        
+        
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageBox.image = UIImage(named: "image1")
 
+        }
+        else{
+            messageLabel.text = awesomeMessage
+            messageLabel.textAlignment = .center
+            messageLabel.textColor = .magenta
+            imageBox.image = UIImage(named: "image0")
+
+        }
+        
+    }
+    
 
 }
 
