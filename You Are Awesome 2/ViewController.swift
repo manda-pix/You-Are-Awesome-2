0fb0f8c5272ede28dc0924d47302f6ca617bf02b
Amanda Guinyard <manda4066@gmail.com>
2021-06-08 09:34:53 -0400

    Initial Commit

---
+++ You Are Awesome 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome 2
+//
+//  Created by amanda guinyard on 6/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
